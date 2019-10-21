44a7e5fa6dab123617717850b597624857142733
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-22 10:02:43 +1300

    added in bootstrap and scripts to the functions file. changed content to output on cards

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+
+    <div class="container">
+        <?php if (have_posts()): ?>
+            <?php while (have_posts()): the_post(); ?>
+                <div class="card mt-3">
+                    <h5 class="card-header"><?php the_title(); ?></h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <div>
+                            <?php the_content(); ?>
+                        </div>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
