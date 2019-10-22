70780edc1e8f286b41593548bcb967d79a30d9c3
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-23 11:39:53 +1300

    added in top navigation bar with bootstrap styling. Included ability to style with bootstrap

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ function addCustomThemeFiles_1902(){
     wp_enqueue_style('bootstrapCSS1902', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '4.3.1', 'all');
     wp_enqueue_style('customCSS1902', get_template_directory_uri() . '/assets/css/style.css', array(), '0.0.2', 'all');
 
-
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrapJS1902', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '4.3.1', true);
     wp_enqueue_script('customJS1902', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '0.0.1', true);
@@ -15,3 +14,21 @@ add_action('wp_enqueue_scripts', 'addCustomThemeFiles_1902');
 
 
 add_theme_support('post-thumbnails');
+
+add_image_size('icon', 50, 50, true);
+
+function addCustomMenus_1902(){
+    add_theme_support('menus');
+    // register_nav_menu('top_navigation', 'This is the navigation which is located at the top of each page.);
+    register_nav_menu('top_navigation', __('The Top navigation is the navigation located at the top of each page.', '1902Custom'));
+    register_nav_menu('side_navigation', __('The Side navigation is the navigation located at the side of each page', '1902Custom'));
+    register_nav_menu('bottom_navigation', __('The Bottom navigation is the navigation located at the bottom of each page', '1902Custom'));
+}
+
+add_action('after_setup_theme', 'addCustomMenus_1902');
+
+function register_navwalker(){
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+
+add_action( 'after_setup_theme', 'register_navwalker' );
--- index.php
+++ index.php
@@ -6,6 +6,35 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <?php if (has_nav_menu('top_navigation')): ?>
+        <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">Navbar</a>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'  => 'top_navigation',
+                    'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                    'container'       => 'div',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id'    => 'top_navigation',
+                    'menu_class'      => 'navbar-nav mr-auto ',
+                    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'          => new WP_Bootstrap_Navwalker(),
+                ) );
+                ?>
+            </div>
+        </nav>
+    <?php endif; ?>
+    <?php if (has_nav_menu('side_navigation')): ?>
+
+    <?php endif; ?>
+    <?php if (has_nav_menu('bottom_navigation')): ?>
+
+    <?php endif; ?>
 
     <div class="container">
         <?php if (have_posts()): ?>
@@ -13,26 +42,30 @@
                 <div class="card mt-3">
                     <h5 class="card-header"><?php the_title(); ?></h5>
                     <div class="card-body">
-
-                        <h5 class="card-title">Special title treatment</h5>
                         <div>
                             <?php if (!is_single()): ?>
                                 <?php if (has_post_thumbnail()): ?>
                                     <div class="row">
-                                        <?php the_post_thumbnail('medium', ['class' => 'col col-4']); ?>
+                                        <div class="col-12 col-md-4">
+                                            <?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+                                        </div>
+
                                         <div class="col-8">
                                             <?php the_excerpt(); ?>
                                             <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
                                         </div>
 
                                     </div>
-                                    <?php else: ?>
-                                        <?php the_excerpt(); ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
+                                <?php else: ?>
+                                    <?php the_excerpt(); ?>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
                                 <?php endif; ?>
                             <?php else: ?>
                                 <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
-                                <?php the_content(); ?>
+                                <div>
+                                    <?php the_content(); ?>
+                                </div>
+
                             <?php endif; ?>
 
                         </div>
