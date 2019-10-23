851f775f67de3009f5b065910603a67498819c2f
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-24 09:07:57 +1300

    added in the footer and side nav bars

--- index.php
+++ index.php
@@ -34,20 +34,15 @@
 
     <div class="container">
         <?php if (has_nav_menu('side_navigation')): ?>
-            <div class="row">
+            <div class="row mt-3">
                 <div class="col-12 col-md-3">
-                    <?php wp_nav_menu( array(
-                        'theme_location' => 'side_navigation',
-                        'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-                        'container'       => 'div',
-                        'container_class' => 'list-group',
-                        'container_id'    => 'side_navigation',
-                        'menu_class'      => 'list-group-item',
-                        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker'          => new WP_Bootstrap_Navwalker(),
-                    )); ?>
+                    <div class="card h-100 mb-2 mt-2 p-2">
+                        <?php wp_nav_menu( array(
+                            'theme_location' => 'side_navigation',
+                            )); ?>
+                    </div>                    
                 </div>
-                <div class="col-12 col-md-9">
+                <div class="col">
                     <?php if (have_posts()): ?>
                         <?php while (have_posts()): the_post(); ?>
                             <div class="card mt-3">
@@ -125,7 +120,27 @@
 
     </div>
     <?php if (has_nav_menu('bottom_navigation')): ?>
-
+        <nav class="navbar navbar-expand-md navbar-light bg-light fixed-bottom" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">Navbar</a>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'  => 'bottom_navigation',
+                    'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                    'container'       => 'div',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id'    => 'bottom_navigation',
+                    'menu_class'      => 'navbar-nav mr-auto ',
+                    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'          => new WP_Bootstrap_Navwalker(),
+                ) );
+                ?>
+            </div>
+        </nav>
     <?php endif; ?>
 
     <?php wp_footer(); ?>
