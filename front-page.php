bf0bdcf6c86996b55ef5cb2befe16ea4a35f2de7
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-29 11:56:02 +1300

    added in templates for each post type. added in ability to see first video on post card

--- front-page.php
+++ front-page.php
@@ -20,47 +20,11 @@
 
 
 <?php if (have_posts()): ?>
-    <div class="container">
+    <div class="container py-5">
         <div class="row justify-content-center">
             <?php while (have_posts()): the_post(); ?>
-                <div class="col-12 col-md-4">
-                    <div class="card  m-1 mt-3">
-                        <h5 class="card-header"><?php the_title(); ?></h5>
-                        <?php if (has_post_thumbnail()): ?>
-
-                            <?php the_post_thumbnail('medium', ['class' => 'card-img-top']); ?>
-                        <?php endif; ?>
-
-                        <div class="card-body">
-                            <!-- <p><?php echo get_post_format(); ?></p> -->
-                            <div>
-                                <?php if (!is_singular()): ?>
-                                    <?php the_excerpt(); ?>
-                                    <?php if (has_post_format('video')): ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-warning">Watch Video</a>
-                                    <?php elseif (has_post_format('audio')): ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-success">Listen Here</a>
-                                    <?php elseif (has_post_format('image')): ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-info">View Image</a>
-                                    <?php elseif (has_post_format('gallery')): ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-danger">View Gallery</a>
-                                    <?php else: ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
-
-                                    <?php endif; ?>
-
-                                <?php else: ?>
-                                    <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
-                                    <div>
-                                        <?php the_content(); ?>
-                                    </div>
-
-                                <?php endif; ?>
-
-                            </div>
-
-                        </div>
-                    </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <?php get_template_part('templates/content', get_post_format()); ?>
                 </div>
 
 
---
+++ templates/content-video.php
@@ -0,0 +1,31 @@
+<?php
+    if (has_blocks()) {
+        $allBlocks = parse_blocks(get_the_content());
+        for ($i=0; $i < count($allBlocks); $i++) {
+            // echo "<pre>";
+            // print_r($allBlocks[$i]);
+            // echo "</pre>";
+                // echo "Block number" .$i. ' is a ' . $allBlocks[$i]['blockName'];
+                // echo '<br>';
+                if ($allBlocks[$i]['blockName'] == 'core-embed/youtube') {
+                    // echo "this is a video block";
+                    $firstVideoBlock = $allBlocks[$i];
+                    break;
+                }
+        }
+        // var_dump($firstVideoBlock);
+        // die();
+    };
+?>
+
+<div class="card h-100 border border-warning">
+    <div class="card-body">
+        <h5 class="card-header"><?php the_title(); ?></h5>
+        <?php if ($firstVideoBlock): ?>
+            <div class="fullVideo">
+                <?php echo apply_filters( 'the_content', render_block($firstVideoBlock)) ; ?>
+            </div>
+        <?php endif; ?>
+        <a href="<?php the_permalink(); ?>" class="btn btn-warning btn-block">Watch Video</a>
+    </div>
+</div>
